aa543a5e044da013acd98dab246e01567b293f06
NeverTh <neverth0@foxmail.com>
2020-08-22 00:19:14 +0800

    由maven转为gradle

---
+++ test/src/main/java/ApplicationMain.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author NeverTh
+ * @date 0:00 2020/8/22
+ */
+@SpringBootApplication
+public class ApplicationMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationMain.class, args);
+    }
+}
