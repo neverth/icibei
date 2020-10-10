ea12d9e6484599639e8157a5e28cad115e49f680
neverth.li <neverth.li@vipshop.com>
2020-10-10 17:37:18 +0800

    添加四级单词的crud

---
+++ organization/src/main/java/fun/neverth/icibei/organization/dao/WordsCet4Mapper.java
@@ -0,0 +1,15 @@
+package fun.neverth.icibei.organization.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import fun.neverth.icibei.organization.entity.po.WordsCet4;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author neverth.li
+ * @date 2020/10/10 16:06
+ */
+@Repository
+@Mapper
+public interface WordsCet4Mapper extends BaseMapper<WordsCet4> {
+}
---
+++ organization/src/main/java/fun/neverth/icibei/organization/entity/param/WordsCet4QueryParam.java
@@ -0,0 +1,11 @@
+package fun.neverth.icibei.organization.entity.param;
+
+import lombok.Data;
+
+/**
+ * @author neverth.li
+ * @date 2020/10/10 16:18
+ */
+@Data
+public class WordsCet4QueryParam {
+}
