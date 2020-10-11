d31f3df98ecf16d9907c6f9c11f3e5a6ecabbab7
NeverTh <neverth0@foxmail.com>
2020-10-11 14:53:56 +0800

    跨域，添加代码生成器

---
+++ organization/src/main/java/fun/neverth/icibei/organization/entity/vo/UserInfoVO.java
@@ -0,0 +1,45 @@
+package fun.neverth.icibei.organization.entity.vo;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author NeverTh
+ * @date 13:17 2020/10/11
+ */
+@Data
+public class UserInfoVO {
+    /**
+     * 唯一Id
+     */
+    private String user_id;
+    /**
+     * 账户ID，唯一
+     */
+    private String accountId;
+    /**
+     * 真实姓名
+     */
+    private String realName;
+    /**
+     * 手机号
+     */
+    private String mobile;
+    /**
+     * 别名
+     */
+    private String nickName;
+    /**
+     * 个性签名
+     */
+    private String signature;
+    /**
+     * 头像网址
+     */
+    private String avatar;
+    /**
+     * 角色
+     */
+    private List<String> roles;
+}
