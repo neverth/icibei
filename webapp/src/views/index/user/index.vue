14d0f961c5aaa5f4573295705648f46f68425960
NeverTh <neverth0@foxmail.com>
2020-10-21 18:31:39 +0800

    完全同步mavenGoGradleNew分支

--- webapp/src/views/index/user/index.vue
+++ webapp/src/views/index/user/index.vue
@@ -43,5 +43,6 @@ body, html {
   background-color: #082730;
   font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
   color: white;
+  height: auto;
 }
 </style>
