2b3c0fbe86ef17e402f9f490d991ace4fdb33c99
NeverTh <neverth0@foxmail.com>
2020-11-13 21:36:30 +0800

    后端区分不同环境

--- game-server/src/main/java/fun/neverth/icibei/gameserver/GameServerMain.java
+++ game-server/src/main/java/fun/neverth/icibei/gameserver/GameServerMain.java
@@ -2,12 +2,14 @@ package fun.neverth.icibei.gameserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author NeverTh
  * @date 19:16 2020/11/9
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GameServerMain {
     public static void main(String[] args) {
         SpringApplication.run(GameServerMain.class, args);
