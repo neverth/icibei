fdd1f631edc08847a4dc5d3c69b04fcfa7ccc268
NeverTh <neverth0@foxmail.com>
2020-12-11 20:13:03 +0800

    action test

--- script/workflow/diffProject.py
+++ script/workflow/diffProject.py
@@ -1,4 +1,27 @@
+# coding=utf-8
 import os
+import paramiko
+
+ssh = paramiko.SSHClient()
+
+# 允许连接不在~/.ssh/known_hosts文件中的主机
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+# 连接服务器
+ssh.connect(hostname="106.14.44.108", port=22, username="root", password="Ly19980911")
+
+# 执行命令，不要执行top之类的在不停的刷新的命令(可以执行多条命令，以分号来分隔多条命令)
+stdin, stdout, stderr = ssh.exec_command("ls -la")
+
+# 获取命令结果
+res, err = stdout.read(), stderr.read()
+stdin.write("pwd\n")
+stdin.flush()q
+result = res if res else err
+res = result.decode()
+res = result.decode("utf-8")
+res = result.decode(encoding="utf-8")
+print res
 
 PROJECTS_DIR = [
     'authentication-server',
@@ -18,10 +41,14 @@ def get_modify_project():
     tags = os.popen('git tag').read().split('\n')
     tags.sort(reverse=True)
 
-    modify_files = os.popen('git diff --stat {} {}'.format(tags[0], tags[1])).read().split('\n')
+    modify_files = os.popen('git diff -X --dirstat=files  {} {}'.format(tags[0], tags[1])).read().split('\n')
 
-    for modify_file in modify_files[0: -2]:
-        dir_name = modify_file.split('/')[0]
+    for modify_file in modify_files:
+
+        if modify_file.find('%') == -1:
+            continue
+
+        dir_name = modify_file.split('%')[1].strip().split('/')[0]
 
         if dir_name in PROJECTS_DIR:
             modify_project.append(dir_name)
@@ -30,4 +57,5 @@ def get_modify_project():
 
 
 if __name__ == '__main__':
-    get_modify_project()
+    print get_modify_project()
+    os.popen('cd ../../ && mvn package')
